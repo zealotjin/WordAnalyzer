466b7b51c9a8670df50ad48f5f9a3ec76f823322
JinGeun Lee <Jin@JinGeuns-MacBook-Pro.local>
2015-02-23 16:54:25 -0800

    initial commit

---
+++ WordAnalyzer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordAnalyzer
+//
+//  Created by JinGeun Lee on 2/23/15.
+//  Copyright (c) 2015 JGProduction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordAnalyzer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordAnalyzer
+//
+//  Created by JinGeun Lee on 2/23/15.
+//  Copyright (c) 2015 JGProduction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
